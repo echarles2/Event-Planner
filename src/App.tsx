1b5dda45200d6c1c1bcf1f064a5b6eb60244b02b
juliadimayuga <jdimayuga3@academic.rrc.ca>
2026-01-24 10:07:18 -0600

    Updated App.tsx to use routes at different paths.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import "./App.css";
-import Header from './components/header/Header'
+import {Routes, Route} from "react-router-dom";
+import Header from './components/layout/header/Header'
 import CreateEvent from "./components/create-event/CreateEvent";
 import CalendarComponent from "./components/calendar-component/calendar-component";
-import Footer from "./components/footer/Footer";
+import Footer from "./components/layout/footer/Footer";
+import Landing from "./components/landing/Landing";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CreateEvent />
-      <CalendarComponent />
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/calendar" element={<CalendarComponent />}></Route>
+        <Route path="/create-event" element={<CreateEvent />}></Route>
+      </Routes>
       <Footer />
     </main>
   );
