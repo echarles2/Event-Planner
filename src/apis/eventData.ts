cf7ad453e45ba8d0e70c1471edfe999619fffd55
juliadimayuga <jdimayuga3@academic.rrc.ca>
2026-02-13 18:08:03 -0600

    Created test data and repository to manage data storage and access.

---
+++ src/apis/eventData.ts
@@ -0,0 +1,14 @@
+import type {Event} from "../components/create-event/CreateEvent";
+
+export const eventTestData: Event[] = [
+    {id: 1, name: "Jane's Birthday Party", date: "2026-04-01", location: "123 ABC Street", details: ["Bring a gift"]},
+    {id: 2, name: "Christmas Celebration", date: "2026-12-25", location: "321 XYZ Avenue", details: ["Wear red/green"]},
+    {id: 3, name: "Halloween Party", date: "2026-10-31", location: "456 Haunted House", details: ["Wear a costume"]},
+    {id: 4, name: "Valentine's Day Dinner", date: "2026-02-14", location: "654 Restaurant", details: ["Wear pink"]},
+    {id: 5, name: "Easter Egg Hunt", date: "2026-04-05", location: "The Backyard", details: ["Bring a basket"]},
+    {id: 6, name: "John's Retirement Party", date: "2026-05-01", location: "789 Community Centre", details: ["Bring food to share"]},
+    {id: 7, name: "Game Night", date: "2026-07-01", location: "Board Game Store", details: ["Bring board games"]},
+    {id: 8, name: "Gift Exchange", date: "2026-12-24", location: "987 ABC Street", details: ["Don't forget a present"]},
+    {id: 9, name: "Potluck Dinner", date: "2026-06-01", location: "234 XYZ Avenue", details: ["Cook or bake something"]},
+    {id: 10, name: "Beach Trip", date: "2026-08-01", location: "Meet at the beach", details: ["Bring snacks"]}
+];
