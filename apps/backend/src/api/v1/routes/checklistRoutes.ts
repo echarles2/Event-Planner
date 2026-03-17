a57abd104b909420a02df4cf6ca2ddb3ccb2fb4e
Lalaine Balmediano <lbalmediano3@academic.rrc.ca>
2026-03-17 18:44:06 -0500

    Implemented routes and controllers for the checklist

--- apps/backend/src/api/v1/routes/checklistRoutes.ts
+++ apps/backend/src/api/v1/routes/checklistRoutes.ts
@@ -1,6 +1,22 @@
 import express, {Router} from "express";
 import * as checklistControllers from "../controllers/checklistControllers";
 import { validateRequest } from "../middleware/validate";
-import { checklistSchema } from "../validations/checklistValidation";
+import { checklistSchema, checklistItemSchema } from "../validations/checklistValidation";
 
 const router: Router = express.Router();
+// For checklist group
+router.get("/checklists", checklistControllers.getAllChecklists);
+
+router.post("/checklists", validateRequest(checklistSchema),
+    checklistControllers.createChecklist);
+
+router.delete("/checklists/:id", checklistControllers.deleteChecklist);
+// For checklist item
+router.post("/checklist-items", validateRequest(checklistItemSchema),
+    checklistControllers.createChecklistItem);
+
+router.patch("/checklist-items/:id", checklistControllers.updateChecklistItem);
+
+router.delete("/checklist-items/:id", checklistControllers.deleteChecklistItem);
+
+export default router;
