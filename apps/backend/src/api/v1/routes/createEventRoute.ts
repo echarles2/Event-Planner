26d2bee8ad043086db1a899b9f7293bea46c3599
juliadimayuga <jdimayuga3@academic.rrc.ca>
2026-03-27 16:05:23 -0500

    Updated post route to include event validation schema.

--- apps/backend/src/api/v1/routes/createEventRoute.ts
+++ apps/backend/src/api/v1/routes/createEventRoute.ts
@@ -1,9 +1,11 @@
 import express, { Router } from "express";
 import * as eventController from "../controllers/createEventController";
+import { validateRequest } from "../middleware/validate";
+import { eventSchema } from "../validations/eventValidation";
 
 const router: Router = express.Router();
 
 router.get("/create-event", eventController.getAllEvents);
-router.post("/create-event", eventController.createEvent);
+router.post("/create-event", validateRequest(eventSchema), eventController.createEvent);
 
 export default router;
