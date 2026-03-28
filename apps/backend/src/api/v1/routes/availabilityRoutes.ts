22dc5c91276ce1c730d5b1b9d7058b806d1486ba
ethan <echarles2@academic.rrc.ca>
2026-03-27 21:59:21 -0500

    added validation back to availability post endpoint

--- apps/backend/src/api/v1/routes/availabilityRoutes.ts
+++ apps/backend/src/api/v1/routes/availabilityRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { fetchAvailability, createOrUpdateAvailability, deleteAvailability} from "../controllers/availabilityController.js";
-//import { availabilitySchema } from "../validations/availabilityValidation.js";
-//import { validateRequest } from "../middleware/validate.js";
+import { availabilitySchema } from "../validations/availabilityValidation.js";
+import { validateRequest } from "../middleware/validate.js";
 
 const router = Router();
 
 router.get("/availability", fetchAvailability);
-router.post("/availability", createOrUpdateAvailability);
+router.post("/availability", validateRequest(availabilitySchema), createOrUpdateAvailability);
 router.delete("/availability/:date", deleteAvailability);
 
 export default router;
